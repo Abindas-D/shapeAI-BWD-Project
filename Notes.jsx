3dc755d4c0da9d2120cad42ec97c0e94a2258820
Abindas-D <49938108+Abindas-D@users.noreply.github.com>
2021-06-05 16:01:32 +0530

    Add files via upload

---
+++ Notes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This was an amazing bootcamp taken up by Shaurya Sinha. We covered
+        evrything from scratch including Javascript, React.js, HTML.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
